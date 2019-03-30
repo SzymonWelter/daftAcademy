c99a8c5dd11df6fadd270e6479d550638fb1c969
SzymonWelter <szymon.welter98@gmail.com>
2019-03-31 00:56:40 +0100

    install sass and create reset

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 var path = require('path');
 var HTMLWebpackPlugin = require('html-webpack-plugin');
+const isProduction = process.env.NODE_ENV === 'production';
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main-bundle.js'
+  },
+  devServer:{
+    contentBase: path.resolve(__dirname, 'dist')
   },
   module: {
     rules:[
@@ -20,10 +25,22 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.s(c|a)ss$/,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader :          
+          {loader: 'style-loader', options: {sourceMap: true}},
+          {loader: 'css-loader', options: {sourceMap: isProduction}},          
+          {loader: 'sass-loader', options: {sourceMap: isProduction}},
+          {loader: 'postcss-loader', options: {sourceMap: isProduction}},
+        ]
       }
     ]
   },
-  plugins: [new HTMLWebpackPlugin()]
+  plugins: [
+    new HTMLWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
 };
